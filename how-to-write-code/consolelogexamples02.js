822449d3aea9a7defb64b6d2b5ce98ba95b63f37
Dominique Gould <dominiquegould42@yahoo.com>
2024-01-06 12:21:41 -0600

    Finished assignment

--- how-to-write-code/consolelogexamples02.js
+++ how-to-write-code/consolelogexamples02.js
@@ -1,3 +1,34 @@
 console.log("Some Programming Languages:");
 
 console.log("Python\nJavaScript\nJava\nC#\nSwift");
+let NameSpaceShuttle = "Determination";
+console.log(NameSpaceShuttle)
+
+console.log("\"Hello World!\"")
+console.log(Number('3'))
+console.log(Number('3 3'))
+console.log(Number('33'))
+console.log(Number('three'))
+console.log(16 - 2 * 5 / 3 + 1)
+console.log(2 ** 2 ** 3 * 3)
+console.log(1 + 5 % 3);
+let ShuttleSpeed = 17500
+console.log(ShuttleSpeed)
+let DistanceMars = 225000000
+console.log(DistanceMars)
+let DistanceMoon = 384400
+console.log(DistanceMoon)
+let MilesKM = 0.621
+console.log(MilesKM)
+
+
+console.log(MilesMars = DistanceMars * MilesKM);
+console.log(HoursMars = MilesMars / ShuttleSpeed);
+console.log(DaysMars = HoursMars / 24);
+
+console.log(NameSpaceShuttle + " will take " + DaysMars + " days to reach Mars.");
+
+console.log(1 + 5 % 3)
+console.log(2 ** 2 ** 3 * 3)
+console.log(16 - 2 * 5 / 3 + 1)
+
